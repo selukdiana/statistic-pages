e3b8e897f0cbe0b3538cbd3941be30ec68264800
Dziyana.Sialiuk <Dziyana.Sialiuk@belstat.local>
2023-04-14 16:08:11 +0300

    created infographics page

--- src/pages/InfographicsPage/InfographicsPage.jsx
+++ src/pages/InfographicsPage/InfographicsPage.jsx
@@ -1,5 +1,13 @@
 import "./InfographicsPage.css";
 
 export const InfographicsPage = () => {
-  return <></>;
+  return <div className="infographics-page">
+    <div className="infographics__grid">
+      <div className="container">
+        <div className="infographics__main">
+          
+        </div>
+      </div>
+    </div>
+  </div>;
 };
